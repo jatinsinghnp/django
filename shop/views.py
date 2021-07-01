a729a58d5b3145fd6156ecbd891b2e842f4cc6cb
jatinsinghnp <js8971105@gmail.com>
2021-07-01 10:17:50 +0545

    add to git

---
+++ shop/views.py
@@ -0,0 +1,35 @@
+
+from django.shortcuts import render,get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Product,Category
+from cart.forms import CartAddProductForm
+from django.views.generic.list import ListView
+
+# Create your views here.
+
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(avaliable=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                avaliable=True)
+    cart_product_form = CartAddProductForm()
+
+    return render(request,'shop/product/detail.html',{'product': product,'cart_product_form': cart_product_form})
